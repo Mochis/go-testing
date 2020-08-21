fe9d813151be9c01c2d31cc6a629312a58ddd4e1
Mochis <juan.mochiesp@gmail.com>
2020-08-22 00:28:43 +0200

    Practice strcuts

--- structs.go
+++ structs.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var arr []int = [...]int {1, 2, 3, 4, 5}
-	// iterate
-	for i, v range
-	(arr) {
-		fmt.Printf("Index[%d], Value: %d", i, v)
+	type Point struct {
+		x int
+		y int
 	}
+
+	var p Point
+	p.x = 1
+	p.y = 1
+	// Create
+	p = *new(Point) // zero type values
+	p.x = 2
+	fmt.Println(p)
+	p = Point{x:1, y:1}
+	fmt.Println(p)
 }
