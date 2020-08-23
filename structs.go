d86c97967d95052005ddbe2d83342f678aecd71e
Mochis <juan.mochiesp@gmail.com>
2020-08-24 01:09:14 +0200

    Structs field visibility and json package

--- structs.go
+++ structs.go
@@ -1,13 +1,18 @@
 package main
 
 import "fmt"
+import "encoding/json"
+
+type Point struct {
+	x int
+	y int
+}
+
+type Square struct {
+	X int
+}
 
 func main() {
-	type Point struct {
-		x int
-		y int
-	}
-
 	var p Point
 	p.x = 1
 	p.y = 1
@@ -17,4 +22,13 @@ func main() {
 	fmt.Println(p)
 	p = Point{x:1, y:1}
 	fmt.Println(p)
+
+	// Struct field visibility
+	s1 := Square{2}
+	p1 := Point{2, 2}
+	s1JSON, _ := json.Marshal(s1) // Square field are seen by json package
+	p1JSON, _ := json.Marshal(p1) // Square field are not visible by json package
+	fmt.Println(string(s1JSON)) // returns s1 json
+	fmt.Println(string(p1JSON)) // returns {}
+	fmt.Println(p1) // returns {2, 2}. Point private fields are visible in same pkg
 }
